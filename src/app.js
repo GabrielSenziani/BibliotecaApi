b1a00fc19974fc08a9edfbcf05a6e9982f6888b0
Gabriel Senziani <senzianigabriel@hotmail.com>
2026-06-25 18:46:48 -0300

    feat: cadastro e login de usuarios com JWT

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import { connectionDB } from "./database/connection.js";
-import router from "./routes/livros.js";
+
+import livroRouter from "./routes/livros.js";
+import usuarioRouter from "./routes/usuario.js"
+
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
-app.use("/livros", router);
 
+app.use("/livros", livroRouter);
+
+app.use("/usuarios", usuarioRouter)
 
 connectionDB().then(() => {
     app.listen(PORT, () => {
---
+++ src/models/Usuario.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const UsuarioSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    }
+});
+
+export const Usuario = mongoose.model("Usuario", UsuarioSchema);
