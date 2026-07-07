40216032106e66f95535c42ff845f9456e0c9e24
Gabriel Senziani <senzianigabriel@hotmail.com>
2026-07-07 14:31:53 -0300

    Corrige porta para deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
 import { connectionDB } from "./database/connection.js";
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 connectionDB().then(() => {
     app.listen(PORT, () => {
