c6eac75e70c6c4254aef631724dd4c59231ff956
Gabriel Senziani <senzianigabriel@hotmail.com>
2026-07-07 14:37:10 -0300

    Corrige porta para deploy

--- src/server.js
+++ src/server.js
@@ -3,8 +3,12 @@ import { connectionDB } from "./database/connection.js";
 
 const PORT = process.env.PORT || 3000;
 
-connectionDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Servidor funcionando na porta ${PORT}`);
+connectionDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Servidor funcionando na porta ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Erro ao conectar ao banco:", err);
     });
-});
